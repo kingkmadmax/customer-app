091075ef70db3668bb0eddf4afbea88b41e06a04
kingkmademax <kirubelmamushet@gmail.com>
2026-06-24 17:02:30 +0300

    making the Product Owner name avater if ther is image ther

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,21 +1,53 @@
-export default function Avatar({ name }: { name: string }) {
-  const initial = name[0].toUpperCase(); // just first letter
+"use client";
 
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+
+import { cn } from "@/lib/utills";
+
+function Avatar({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
   return (
-    <div
-      style={{
-        width: "20px",
-        height: "20px",
-        borderRadius: "50%",
-        backgroundColor: "#4A90E2",
-        color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontWeight: "bold",
-      }}
-    >
-      {initial}
-    </div>
+    <AvatarPrimitive.Root
+      data-slot="avatar"
+      className={cn(
+        "relative flex size-8 shrink-0 overflow-hidden rounded-full",
+        className,
+      )}
+      {...props}
+    />
   );
 }
+
+function AvatarImage({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+  return (
+    <AvatarPrimitive.Image
+      data-slot="avatar-image"
+      className={cn("aspect-square size-full", className)}
+      {...props}
+    />
+  );
+}
+
+function AvatarFallback({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
+  return (
+    <AvatarPrimitive.Fallback
+      data-slot="avatar-fallback"
+      className={cn(
+        "bg-muted flex size-full items-center justify-center rounded-full",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Avatar, AvatarImage, AvatarFallback };
